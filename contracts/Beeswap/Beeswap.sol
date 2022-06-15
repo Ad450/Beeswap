631438bb496972ba0539ad38d4a23acdb41f1aef
Ad450 <adjeimanue@gmail.com>
2022-06-15 16:51:05 +0100

    incopartible versions fixed

--- contracts/Beeswap/Beeswap.sol
+++ contracts/Beeswap/Beeswap.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unilicense
-pragma solidity >=0.4.0;
+pragma solidity ^0.8.4;
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
