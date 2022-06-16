bf2382aba113246349f6e0e1f8a4cc612663be3c
Ad450 <adjeimanue@gmail.com>
2022-06-16 22:08:34 +0100

    test tokens changes

--- contracts/test_token2.sol
+++ contracts/test_token2.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TestToken1 is ERC20{
-    constructor (uint256 _supply) ERC20 ("Test0", "TT0"){
+contract TestToken2 is ERC20{
+    constructor (uint256 _supply) ERC20 ("Test", "TT1"){
         _mint(msg.sender, _supply);
     }
 }
