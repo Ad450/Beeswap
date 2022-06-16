b31842290b09ce88cf923309fcee130e5472702e
Ad450 <adjeimanue@gmail.com>
2022-06-16 22:09:27 +0100

    more testing

--- test/Beeswap_test.ts
+++ test/Beeswap_test.ts
@@ -5,32 +5,35 @@ import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("Beeswap", function () {
-  let testToken0: Contract;
   let testToken1: Contract;
+  let testToken2: Contract;
   let beeswap: Contract;
-  let swapRouter: string = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+  const testAmount: Number = 50;
+  const swapRouter: string = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+  const deployingAddress: string = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+
 
   beforeEach(async () => {
     // token0
-    const TestToken0 = await ethers.getContractFactory("TestToken0");
-    testToken0 = await TestToken0.deploy("500");
-    await testToken0.deployed();
+    const TestToken1 = await ethers.getContractFactory("TestToken1");
+    testToken1 = await TestToken1.deploy(500);
+    await testToken1.deployed();
 
     // token1
-    const TestToken1 = await ethers.getContractFactory("TestToken1");
-    testToken1 = await TestToken0.deploy("500");
-    await testToken0.deployed();
+    const TestToken2 = await ethers.getContractFactory("TestToken2");
+    testToken2 = await TestToken2.deploy(50);
+    await testToken2.deployed();
 
     // deploy beeswap contract
     const Beeswap = await ethers.getContractFactory("Beeswap");
-    beeswap = await Beeswap.deploy(swapRouter, testToken0.address, testToken1.address, 0, 2000);
+    beeswap = await Beeswap.deploy(swapRouter, testToken1.address, testToken1.address, 0, 10);
 
     await beeswap.deployed();
   });
 
   it("Should deploy Beeswap", async () => {
 
-    expect(testToken0.address).exist;
+    expect(testToken1.address).exist;
     expect(testToken1.address).exist;
 
     expect(beeswap.address).exist;
@@ -38,10 +41,14 @@ describe("Beeswap", function () {
   });
 
   it("should return the maximum amount of token1 in swap", async () => {
-    testToken0.balanceOf()
+    // first load the balance of the deploying contract with at least 50
+    expect(await testToken1.balanceOf(deployingAddress)).equals(500)
 
-    // const result = await beeswap.swapExactInput(3000);
+    // approve beeswap to spend tokens
+    testToken1.approve(beeswap.address, testAmount);
 
-    //assert(result !== null);
+    await beeswap.swapExactInput(testAmount);
+
+    // assert(result !== null);
   })
 });
