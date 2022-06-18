563b5888e49c6acd148337e94b16a9a31da4641a
Ad450 <adjeimanue@gmail.com>
2022-06-18 16:38:53 +0100

    using waffle test suite to deploy beeswap

--- test/Beeswap_test.ts
+++ test/Beeswap_test.ts
@@ -1,54 +1,31 @@
 
 import { expect } from "chai";
 import { assert } from "console";
+import { deployContract, MockProvider } from "ethereum-waffle";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
+import BeeswapJson from "../artifacts/contracts/Beeswap/Beeswap.sol/Beeswap.json";
+import Token1Json from "../artifacts/contracts/test_token1.sol/TestToken1.json";
+import Token2Json from "../artifacts/contracts/test_token2.sol/TestToken2.json";
 
 describe("Beeswap", function () {
-  let testToken1: Contract;
-  let testToken2: Contract;
-  let beeswap: Contract;
-  const testAmount: Number = 50;
   const swapRouter: string = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
-  const deployingAddress: string = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-
+  const [walletFrom, walletTo] = new MockProvider().getWallets();
+  let beeswap: Contract;
+  let token1: Contract;
+  let token2: Contract;
 
   beforeEach(async () => {
-    // token0
-    const TestToken1 = await ethers.getContractFactory("TestToken1");
-    testToken1 = await TestToken1.deploy(500);
-    await testToken1.deployed();
+    token1 = await deployContract(walletFrom, Token1Json, [500]);
+    token2 = await deployContract(walletTo, Token2Json, [500]);
+    beeswap = await deployContract(walletFrom, BeeswapJson, [swapRouter, token1.address, token2.address, 0, 10]);
 
-    // token1
-    const TestToken2 = await ethers.getContractFactory("TestToken2");
-    testToken2 = await TestToken2.deploy(50);
-    await testToken2.deployed();
 
-    // deploy beeswap contract
-    const Beeswap = await ethers.getContractFactory("Beeswap");
-    beeswap = await Beeswap.deploy(swapRouter, testToken1.address, testToken1.address, 0, 10);
-
-    await beeswap.deployed();
   });
 
-  it("Should deploy Beeswap", async () => {
-
-    expect(testToken1.address).exist;
-    expect(testToken1.address).exist;
-
+  it("should deploy beeswap", async () => {
     expect(beeswap.address).exist;
-
-  });
-
-  it("should return the maximum amount of token1 in swap", async () => {
-    // first load the balance of the deploying contract with at least 50
-    expect(await testToken1.balanceOf(deployingAddress)).equals(500)
-
-    // approve beeswap to spend tokens
-    // testToken1.approve(beeswap.address, testAmount);
-
-    await beeswap.swapExactInput(testAmount);
-
-    // assert(result !== null);
   })
+
+
 });
