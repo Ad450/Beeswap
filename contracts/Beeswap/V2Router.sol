1c6d0b3e47e5ae2c01943774c352083b6f9b1504
Ad450 <adjeimanue@gmail.com>
2022-06-19 15:42:41 +0100

    v2 interface

---
+++ contracts/Beeswap/V2Router.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IV2Router {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function WETH() external pure returns (address);
+}
