53d4c551e15c40a01dc23db477691b6ca4ba00b9
Ad450 <adjeimanue@gmail.com>
2022-06-21 08:07:30 +0100

    default network set to rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "@tenderly/hardhat-tenderly"
 
 dotenv.config();
 
@@ -23,13 +24,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity:"0.8.4",
+  solidity: {
+    compilers: [
+      { version: "0.8.4" },
+      { version: "=0.6.6" }
+    ]
+  },
+  defaultNetwork: "rinkeby",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
