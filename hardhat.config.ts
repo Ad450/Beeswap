a11b47c80b8011e77cedd9cf9c1deba13534e8fb
Ad450 <adjeimanue@gmail.com>
2022-06-26 15:57:25 +0100

    minor changes in version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,7 +30,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       { version: "0.8.4" },
-      { version: "=0.6.6" }
+      { version: "0.6.6" }
     ]
   },
   defaultNetwork: "rinkeby",
