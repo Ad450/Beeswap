847599f6af4b0d0874d62edf4869d0542f5d3611
Ad450 <adjeimanue@gmail.com>
2022-06-28 13:30:08 +0100

    first test of swapEtHforTokens passes

--- test/Beeswap_test.ts
+++ test/Beeswap_test.ts
@@ -2,68 +2,36 @@
 import { expect } from "chai";
 import { deployContract, MockProvider } from "ethereum-waffle";
 import { BigNumber, Contract } from "ethers";
-import BeeswapJson from "../artifacts/contracts/Beeswap/Beeswap.sol/Beeswap.json";
 import Token1Json from "../artifacts/contracts/test_token1.sol/TestToken1.json";
 import Token2Json from "../artifacts/contracts/test_token2.sol/TestToken2.json";
-import BeeJson from "../artifacts/contracts/Beeswap/Bee.sol/Bee.json";
-import ExampleJson from "../artifacts/contracts/Beeswap/example.sol/SwapExamples.json";
-import V2ExampleJson from "../artifacts/contracts/Beeswap/V2_example.sol/V2Example.json";
+import BeeswapV2Json from "../artifacts/contracts/Beeswap/Beewsap_v2.sol/BeeswapV2.json";
+import BeeswapV3json from "../artifacts/contracts/Beeswap/Beeswap_v3.sol/BeeswapV3.json";
+import { ethers } from "hardhat";
 
 describe("Beeswap", function () {
   const v2Router: string = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
   const v3Router: string = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+  const signerAddress: string = "0xe57A9202878B529E6D35602faB66a5a0B5143f38";
   const [walletFrom, walletTo] = new MockProvider().getWallets();
   const totalSupply: BigInt = BigInt(500 * 10 ** 18);
-  let beeswap: Contract;
   let token1: Contract;
   let token2: Contract;
-  let bee: Contract;
-  let example: Contract;
-  let v2Example: Contract;
+  let beeswapV2: Contract;
+  let beeswapv3: Contract;
 
   beforeEach(async () => {
     token1 = await deployContract(walletFrom, Token1Json, [totalSupply]);
     token2 = await deployContract(walletTo, Token2Json, [totalSupply]);
-    beeswap = await deployContract(walletFrom, BeeswapJson,);
-    bee = await deployContract(walletFrom, BeeJson)
-    example = await deployContract(walletFrom, ExampleJson, [v3Router]);
-    v2Example = await deployContract(walletFrom, V2ExampleJson);
+
+    //  get signer from address 
+    const signer = await ethers.getSigner(signerAddress);
+    beeswapV2 = await deployContract(signer, BeeswapV2Json);
+
   });
 
-  // it("should deploy beeswap", async () => {
-  //   expect(beeswap.address).exist;
-  // })
-
-  // it("should return 500 token1 for walletFrom", async () => {
-  //   //expect(await token1.balanceOf(walletFrom.address)).equals(500);
-  //   console.log(beeswap.address);
-
-  // })
-
-  // // test swap functionality without LPs, load Beeswap with some token2
-  it("should swap token1 and token2", async () => {
-
-    await token1.approve(beeswap.address, 300, { from: walletFrom.address });
-
-    const results = await beeswap.swapExactInput(25, token1.address, token2.address, { from: walletFrom.address, gasLimit: 3000000 });
-    expect(await token1.balanceOf(beeswap.address)).equals(25);
-    expect(await token1.allowance(beeswap.address, v3Router)).equals(25);
-    // console.log(results);
-
-  })
-
-
-  it("v2 example ", async () => {
-    token1.approve(v2Example.address, 200, { from: walletFrom.address });
-    const results = await v2Example.swapTokensToETH(10, token1.address, { from: walletFrom.address });
+  it("Beeswapv2 swapETHforTokens", async () => {
+    const results = await beeswapV2.swapEthForTokens({ from: signerAddress, value: 2000, gasLimit: 3000000 });
     console.log(results);
 
-  })
-
-  // not part of contract mian test cases
-  it("should get weth address", async () => {
-    const results = await beeswap.testWeth({ from: walletFrom.address, gasLimit: 3000000 });
-    console.log(results);
-
-  })
+  }).timeout(70000)
 });
